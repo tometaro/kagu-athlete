de52cfde363a014964ccc815963c6f93bfdeb515
留岡佑太郎 <>
2019-01-24 22:31:32 +0900

    電話番号

--- template-parts/cvArea.php
+++ template-parts/cvArea.php
@@ -1,7 +1,7 @@
 <div class="cvArea fadeIn">
   <div class="cvArea-wrap">
     <div class="cvArea-title">お見積もり相談</div>
-    <a class="button cvArea-button" href="tel:07053475596">電話する / 070-5347-5596</a>
+    <a class="button cvArea-button" href="tel:0787851159">電話する / 078-785-1159</a>
     <div class="cvArea-buttonWrap">
       <a class="button button-ghost cvArea-button" href="https://line.me/ti/p/Lhx35etWMf">LINEする</a>
       <a class="button button-ghost cvArea-button" href="/index.php?page_id=40">メールする</a>
